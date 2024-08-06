3cafc0e7771a6c855c0ecc002e40ebd48e689479
Burak <burakaliunlu@gmail.com>
2024-08-06 06:22:17 -0400

    Initial Commit

---
+++ gpacalculator/gpacalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpacalculatorApp.swift
+//  gpacalculator
+//
+//  Created by Burak Unlu on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct gpacalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
